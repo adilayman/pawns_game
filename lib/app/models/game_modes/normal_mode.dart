086a7fc3fcd30e449f766edbe431e5ebbd8faeb2
Ayman Adil <adilayman53@gmail.com>
2021-07-06 05:35:23 +0200

    soccer score top bar (start)

--- lib/app/models/game_modes/normal_mode.dart
+++ lib/app/models/game_modes/normal_mode.dart
@@ -15,16 +15,36 @@ class NormalMode extends Game {
   @override
   void render(Canvas canvas, Size size) {
     Paint paint = Paint()
-      ..color = Colors.green.shade700
+      ..color = Colors.green.shade800
       ..style = PaintingStyle.fill;
-    canvas.drawRect(Offset.zero & size, paint);
+    canvas.drawRect(
+        Offset(size.width * 0.1, size.height * 0.2) &
+            Size(
+                size.width - size.width * 0.2, size.height - size.height * 0.2),
+        paint);
 
-    Size scoreBarSize = Size(size.width, 65);
-    paint.color = Colors.white70;
+    Size scoreBarSize = Size(size.width, size.height * 0.2);
+
+    paint
+      ..color = Colors.grey.shade100
+      ..style = PaintingStyle.fill;
+    canvas.drawRect(Offset.zero & scoreBarSize, paint);
+
+    paint.shader = LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [
+        Colors.red.shade400,
+        Colors.grey,
+        Colors.blue.shade400,
+      ],
+    ).createShader(Offset.zero & scoreBarSize);
 
     canvas.drawRect(Offset.zero & scoreBarSize, paint);
 
     //
+    paint.shader = null;
+    paint.color = Colors.grey.shade700;
 
     var borders = Rect.fromLTWH(
         0, scoreBarSize.height, size.width, size.height - scoreBarSize.height);
@@ -32,7 +52,7 @@ class NormalMode extends Game {
     final rrectBorders = RRect.fromRectAndRadius(borders, Radius.circular(0));
 
     paint
-      ..color = Colors.white
+      ..color = Colors.grey.shade900
       ..strokeWidth = 5
       ..style = PaintingStyle.stroke;
 
